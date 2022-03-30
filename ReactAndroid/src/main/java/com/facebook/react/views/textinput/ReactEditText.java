975208865941db02dcf75bdbdc8e0c562406e40d
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2022-03-30 12:28:59 +0800

    maybeSetErrorMessage - setError(null) if empty string

--- ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
@@ -511,10 +511,15 @@ public class ReactEditText extends AppCompatEditText
    * @param errorMessage
    */
   public void maybeSetErrorMessage(int eventCounter, String errorMessage) {
-    if (!canUpdateWithEventCount(eventCounter) || getError() == errorMessage) {
+    if (!canUpdateWithEventCount(eventCounter)) {
       return;
     }
-    setError(errorMessage);
+    if (errorMessage.isEmpty() && getError() != null) {
+      setError(null);
+    }
+    if (!errorMessage.isEmpty() && getError() != errorMessage) {
+      setError(errorMessage);
+    }
   }
 
   public void maybeSetTextFromJS(ReactTextUpdate reactTextUpdate) {
