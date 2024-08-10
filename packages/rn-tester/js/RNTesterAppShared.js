b4060e975feb0f332c86c0c7e82225c8132858e5
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2024-08-10 11:34:09 +0800

    adding a second example

--- packages/rn-tester/js/RNTesterAppShared.js
+++ packages/rn-tester/js/RNTesterAppShared.js
@@ -1,17 +1,18 @@
 import * as React from 'react';
-import {
-  Button,
-  Text,
-  TextInput,
-} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 const RNTesterApp = () => {
-  return <TextInputExample />;
+  return (
+    <View style={styles.container}>
+      <TextInputExampleWithChildren />
+    </View>
+  );
 };
 
-function TextInputExample() {
-  // const [value, setValue] = React.useState('\u200b');
-  const [value, setValue] = React.useState('');
+function TextInputExampleWithChildren() {
+  const [value, setValue] = React.useState('\u200b');
+  // Uncomment this to reproduce the issue
+  // const [value, setValue] = React.useState('');
   const [lineHeight, setLineHeight] = React.useState(50);
   const increaseLineHeight = () => {
     setLineHeight(prevLineHeight => prevLineHeight + 10);
@@ -20,15 +21,13 @@ function TextInputExample() {
     setLineHeight(prevLineHeight => prevLineHeight - 10);
   };
 
-  /*
+  // Comment this to reproduce the issue
   React.useEffect(() => {
     if (value.length === 0) {
       setValue('\u200b');
     }
   }, [value]);
-  */
 
-  console.log('lineHeight', lineHeight);
   return (
     <>
       <TextInput
@@ -51,5 +50,52 @@ function TextInputExample() {
     </>
   );
 }
+
+function TextInputExampleLineHeightProp() {
+  const [value, setValue] = React.useState('\u200b');
+  // Comment this to reproduce the issue
+  // const [value, setValue] = React.useState('');
+  const [lineHeight, setLineHeight] = React.useState(50);
+  const increaseLineHeight = () => {
+    setLineHeight(prevLineHeight => prevLineHeight + 10);
+  };
+  const decraseLineHeight = () => {
+    setLineHeight(prevLineHeight => prevLineHeight - 10);
+  };
+
+  React.useEffect(() => {
+    if (value.length === 0) {
+      setValue('\u200b');
+    }
+  }, [value]);
+
+  // Comment this to reproduce the issue
+  return (
+    <>
+      <TextInput
+        multiline
+        style={{
+          backgroundColor: 'red',
+          height: 50,
+        }}
+        lineHeight={lineHeight}
+        value={value}
+        onChangeText={text => setValue(text)}
+      />
+      <Button
+        title="increase line height"
+        onPress={() => increaseLineHeight()}
+      />
+      <Button
+        title="decrease line height"
+        onPress={() => decraseLineHeight()}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {marginTop: 200},
+});
 
 export default RNTesterApp;
