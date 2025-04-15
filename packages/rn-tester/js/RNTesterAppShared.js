aeb8c618b898a9649db352c3f03fe990c3c6f279
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2025-04-15 18:09:48 +0800

    multiline or style update trigger setText update

--- packages/rn-tester/js/RNTesterAppShared.js
+++ packages/rn-tester/js/RNTesterAppShared.js
@@ -9,66 +9,37 @@ const ELLIPSIZE_MODES = {
 
 function RNTesterApp() {
   const [fontSize, setFontSize] = useState(10);
-  const [lineHeight, setLineHeight] = useState(30);
-  const [numberOfLines, setNumberOfLines] = useState(2);
-  const [ellipsizeMode, setEllipsizeMode] = useState(ELLIPSIZE_MODES.HEAD);
-  const [value, setValue] = useState('nested controlled text');
+  const [multiline, setMultiline] = useState();
+
+  const changeMultiline = () => {
+    setMultiline(prev => !prev);
+  };
+
   const changeFontSize = () => {
     setFontSize(prev => prev + 20);
   };
-  const changeLineHeight = () => {
-    setLineHeight(prev => (prev === 30 ? 40 : 30));
-  };
 
-  const nestedTextStyles = {
+  const nestedTextStyles1 = {
     color: 'red',
-    lineHeight,
     fontSize,
+    lineHeight: 10,
+    background: 'blue',
+    letterSpacing: 5,
+    verticalAlign: 'top',
+    textTransform: 'uppercase',
+    writingDirection: 'rtl',
+    userSelect: 'none',
+    textAlign: 'center',
+    fontWeight: 'italic',
   };
 
-  const changeNumberOfLines = () => {
-    setNumberOfLines(prev => (prev === 2 ? 1 : 2));
-  };
-
-  const changeEllipsizeMode = () => {
-    setEllipsizeMode(prev => {
-      switch (prev) {
-        case ELLIPSIZE_MODES.HEAD:
-          return ELLIPSIZE_MODES.TAIL;
-        case ELLIPSIZE_MODES.TAIL:
-          return ELLIPSIZE_MODES.MIDDLE;
-        case ELLIPSIZE_MODES.MIDDLE:
-          return ELLIPSIZE_MODES.HEAD;
-        default:
-          return ELLIPSIZE_MODES.HEAD;
-      }
-    });
-  };
-
-  const changeText = text => {
-    console.log('do nothing');
-    // setText(text);
-  };
-
-  const defaultValue = 'default value never changes';
   return (
     <View style={styles.container}>
-      <Text>numberOfLines: {numberOfLines}</Text>
-      <TextInput
-        multiline
-        numberOfLines={numberOfLines}
-        style={styles.input}
-        onChangeText={changeText}>
-        {' '}
-        <Text ellipsizeMode={ellipsizeMode}>
-          Not controlled<Text style={nestedTextStyles}>{defaultValue}</Text>
-        </Text>
+      <TextInput multiline={multiline} numberOfLines={3} textAlign={'center'}>
+        Not controlled <Text style={nestedTextStyles1}>text1</Text>
       </TextInput>
-      <TextInput>Text Input 2</TextInput>
       <Button title="change font size" onPress={changeFontSize} />
-      <Button title="change line height" onPress={changeLineHeight} />
-      <Button title="change ellipsize mode" onPress={changeEllipsizeMode} />
-      <Button title="change number of lines" onPress={changeNumberOfLines} />
+      <Button title="change multiline" onPress={changeMultiline} />
     </View>
   );
 }
