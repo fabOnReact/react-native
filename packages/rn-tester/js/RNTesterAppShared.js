7bee8320d2a6b5493b3a2bd6201cfb382dc26139
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2025-04-15 18:54:46 +0800

    crash app with 2 Nested Text with controlled state value

--- packages/rn-tester/js/RNTesterAppShared.js
+++ packages/rn-tester/js/RNTesterAppShared.js
@@ -1,16 +1,10 @@
 import React, {useState} from 'react';
 import {View, TextInput, StyleSheet, Button, Text} from 'react-native';
 
-const ELLIPSIZE_MODES = {
-  HEAD: 'head',
-  TAIL: 'tail',
-  MIDDLE: 'middle',
-};
-
 function RNTesterApp() {
   const [fontSize, setFontSize] = useState(10);
   const [multiline, setMultiline] = useState();
-  const [value, setValue] = useState('span');
+  const [value, setValue] = useState('A word. ');
   const [lineHeight, setLineHeight] = useState(10);
   const [letterSpacing, setLetterSpacing] = useState(5);
   const [verticalAlign, setVerticalAlign] = useState('top');
@@ -25,18 +19,26 @@ function RNTesterApp() {
     setFontSize(prev => prev + 20);
   };
 
-  const nestedTextStyles = {
-    color: 'red',
+  const textStyles = {
     fontSize,
     lineHeight,
-    background: 'blue',
     letterSpacing,
     verticalAlign,
     textTransform,
     writingDirection: 'rtl',
     userSelect: 'none',
     textAlign: 'center',
-    fontWeight: 'italic',
+    fontWeight: 'bold',
+  };
+
+  const redTextStyles = {
+    ...textStyles,
+    color: 'red',
+  };
+
+  const greedTextStyles = {
+    ...textStyles,
+    color: 'green',
   };
 
   return (
@@ -48,21 +50,9 @@ function RNTesterApp() {
         style={styles.input}
         onChangeText={onChangeText}>
         A very long long long long long{' '}
-        <Text style={nestedTextStyles}>
+        <Text style={redTextStyles}>
           {value}
-          <Text style={nestedTextStyles}>
-            {value}
-            <Text style={nestedTextStyles}>
-              {value}
-              <Text style={nestedTextStyles}>
-                {value}
-                <Text style={nestedTextStyles}>
-                  {value}
-                  <Text style={nestedTextStyles}>{value}</Text>
-                </Text>
-              </Text>
-            </Text>
-          </Text>
+          <Text style={greedTextStyles}>{value}</Text>
         </Text>
       </TextInput>
       <Button title="change font size" onPress={changeFontSize} />
@@ -74,7 +64,7 @@ function RNTesterApp() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: 'white',
   },
   input: {
     flex: 1,
